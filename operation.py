6d1e7e2bbdf7d7c25a33b4c941b6ea532d228dd7
Mina Y <mina.yousseif@gmail.com>
2019-06-23 14:51:58 -0400

    few other improvements

--- operation.py
+++ operation.py
@@ -1,28 +1,32 @@
-import json, os, logging, time
-from subprocess import Popen, PIPE
-from domains import Domains
+import json, os, logging, time, urllib2
+from subprocess import call, Popen, PIPE
+from pipes import quote
+from config import Config
 from logger  import CustomLogger
+from dotenv import load_dotenv
 
 class Result:
     pass
 
 class AcmeOperation:
+    load_dotenv()
     def __init__(self, provider=None):
         """
         Automate certbot and lexicon to obtain and store 
         let's encrypt ssl certificates into S3-compatible object storage
         """
-        self.logger                     = CustomLogger().logger
-        self.dns_provider               = provider
-        self.dns_provider_username      = None
-        self.dns_provider_auth_token    = None
-        self.dns_provider_update_delay  = 30
-        self.domains                    = Domains(logger=self.logger)
-        self.test                       = False
+        self.logger                    = CustomLogger().logger
+        self.dns_provider              = provider
+        self.dns_provider_username     = os.getenv('DNS_PROVIDER_USERNAME')
+        self.dns_provider_auth_token   = os.getenv('DNS_PROVIDER_AUTH_TOKEN')
+        self.client_ip_address         = self._getPublicIP()
+        self.dns_provider_update_delay = 30
+        self.config                    = Config(logger=self.logger)
+        self.test                      = False
 
     def _providerCheck(self):
-        if self.dns_provider in self.domains.list['domains']:
-            if len(self.domains.list['domains'][self.dns_provider]) > 0:
+        if self.dns_provider in self.config.getconfig['domains']:
+            if len(self.config.getconfig['domains'][self.dns_provider]) > 0:
                 return True
             else:
                 self.logger.error('no domains stored for {0}'.format(self.dns_provider))
@@ -31,25 +35,32 @@ class AcmeOperation:
         self.logger.error("provider is not in the domains list")
         return False
     
+    def _getPublicIP(self):
+        return urllib2.urlopen('https://api.ipify.org/').read()
+
+    def _getToolPath(self, tool):
+        if tool is not None and tool not in self.config.getconfig:
+            return None
+        if 'path' in self.config.getconfig[tool]:
+            return self.config.getconfig[tool]['path']
+
 
     def _runCmd(self, args):
         result  = Result()
 
-        process = Popen(args, stdout=PIPE, stderr=PIPE, shell=True)
-        process.wait()
+        process = Popen(args, stdout=PIPE, stderr=PIPE)
         stdout, stderr = process.communicate()
 
-        result.exitcode = process.wait()
         result.stdout   = stdout
         result.stderr   = stderr
 
-        if process.returncode != 0:
+        if process.returncode:
             self.logger.error('Error executing command {0}'.format(args))
             self.logger.error('StdErr: {0}'.format(stderr))
 
         return result
         
-    def obtain(self, test=False):
+    def obtain(self, test=False, expand=False):
         """
         Obtains the initial letsencrypt certificates for specific domain name provider 
         using manual script hooks to validate the ownership of the domains
@@ -58,56 +69,68 @@ class AcmeOperation:
         certbot certonly --manual --preferred-challenges=dns 
         --manual-auth-hook "/path/to/acmebot.py auth -p namecheap" 
         --manual-cleanup-hook "/path/to/acmebot.py cleanup -p namecheap" 
-        -d sub1.example.com -d sub2.example.com -d another-example.com 
+        -d example.com -d sub2.example.com -d another-example.com 
         --manual-public-ip-logging-ok --noninteractive --agree-tos --test-cert
         """
-        hook_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'acmebot.py')
+        hook_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'acmebot')
         if self._providerCheck():
-            args = ['certbot', 'certonly', '--manual', '--preferred-challenges', 'dns']
+            certbot = self._getToolPath('certbot')
+
+            if certbot is None:
+                self.logger.error("failed to run the certbot cmd, certbot path is not set")
+                return
+
+            args = [certbot, 'certonly', '--manual', '--preferred-challenges', 'dns']
             
-            args.extend(['--manual-auth-hook',"'{0} auth -p {1}'".format(hook_file, self.dns_provider)])
-            args.extend(['--manual-cleanup-hook',"'{0} cleanup -p {1}'".format(hook_file, self.dns_provider)])
-            
-            for domain in self.domains.list['domains'][self.dns_provider]:
+            args.extend(['--manual-auth-hook',quote("{0} auth -p {1}".format(hook_file, self.dns_provider))])
+            args.extend(['--manual-cleanup-hook',quote("{0} cleanup -p {1}".format(hook_file, self.dns_provider))])
+            for domain in self.config.getconfig['domains'][self.dns_provider]:
                 args.extend(['-d', domain])
-            
+                
             # adding certbot options to run in a non interactive mode
             args.extend(['--manual-public-ip-logging-ok', '--noninteractive', '--agree-tos', '--quiet'])   
 
             if test:
-                args.append('--test-cert')    
+                args.append('--test-cert')
+
+            if expand:
+                args.append('--expand')        
             
-            print 'cmd: {0}'.format(' '.join(args))    
-            self._runCmd(args)
+            certbot_cmd = ' '.join(args)
+            # for some reason using Popen does not work with certbot
+            # so we are using os.system for now
+            # TODO: figure our if we can use subprocess.Popen
+            os.system(certbot_cmd)
+            
+            
             
 
     def hook(self, action=None):
         # the passed environment variables from certbot
         CERTBOT_DOMAIN     = os.environ.get("CERTBOT_DOMAIN", None)
         CERTBOT_VALIDATION = os.environ.get("CERTBOT_VALIDATION", None)
-        CERTBOT_TOKEN      = os.environ.get("CERTBOT_TOKEN", None)
-        
-        print "CERTBOT_DOMAIN: {0}".format(CERTBOT_DOMAIN)
-        print "CERTBOT_VALIDATION: {0}".format(CERTBOT_VALIDATION)
-        print "CERTBOT_TOKEN: {0}".format(CERTBOT_TOKEN)
-        
-        self.logger.info("Auth the domain")
+
+        cmd_index = 6
+        lexicon = self._getToolPath('lexicon')
+
+        if lexicon is None:
+            self.logger.error("failed to run the lexicon cmd, lexicon path is not set")
+            return
+
         args = [ 
-            'lexicon', 
+            lexicon, 
             self.dns_provider, 
             '--auth-usernam={0}'.format(self.dns_provider_username), 
             '--auth-token={0}'.format(self.dns_provider_auth_token),
-            'create', 
+            '--auth-client-ip={0}'.format(self.client_ip_address),
+            '--ttl=100',
             CERTBOT_DOMAIN, 
             'TXT', 
             '--name', 
             '_acme-challenge.{0}'.format(CERTBOT_DOMAIN), 
             '--content', 
             CERTBOT_VALIDATION
-            ]
-
-        self._runCmd(args)
-
+        ]
         if action == 'auth':
             #   https://github.com/AnalogJ/lexicon/blob/master/examples/certbot.default.sh#L46
             #   How many seconds to wait after updating your DNS records. This may be required,
@@ -116,6 +139,13 @@ class AcmeOperation:
             #   (e.g. Linode).
             #
             #   Defaults to 30 seconds
-            time.sleep(self.dns_provider_update_delay)           
+            args.insert(cmd_index, 'create')
+            self._runCmd(args)
+            time.sleep(self.dns_provider_update_delay)
+        elif action == 'cleanup':
+            args.insert(cmd_index, 'delete')
+            self._runCmd(args)
+
+
 
 
