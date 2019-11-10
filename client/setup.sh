60c90abba4a27b6114d96af01671c3f05957753c
Mina Yousseif <mina.yousseif@gmail.com>
2019-11-09 21:03:03 -0500

    use cp instead of mv

--- client/setup.sh
+++ client/setup.sh
@@ -5,8 +5,8 @@ echo "Installing dependencies"
 /usr/bin/python3 -m pip install -r requirements.txt
 
 echo "Installing the Acme-Client script and service"
-mv acmebot-client.py /usr/bin/acmebot-client.py
-mv acmebot-client.service /lib/systemd/system/acmebot-client.service
+cp acmebot-client.py /usr/bin/acmebot-client.py
+cp acmebot-client.service /lib/systemd/system/acmebot-client.service
 
 echo "Enable the Acme-Client Service"
 
